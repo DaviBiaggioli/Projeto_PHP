c0b0c6e3edef0f4118e68c2979a71e9bdf0f0dcf
DaviBiaggioli <biaggiolid@gmail.com>
2021-08-19 22:13:18 -0300

    atualizando

---
+++ segundo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<?php
+#comentário
+# data 19.08.2021 
+ ?>
+<html>
+<head>
+	<title>php testendo cometario</title>
+</head>
+<body>
+
+</body>
+<?php 
+	/*
+	comentário de linha
+	*/
+	echo "Segundo exemplo de comentário";
+	$data = "19 de agosto de 2021";
+ ?>
+ <p>
+ 	Este paragrafo esta sendo interpretado pelo html.
+ </p>
+ arquivo criado em php <?php echo $data; ?>
+</html>
